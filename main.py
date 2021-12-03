83c0a3af35759b72f8cf74c5c37d493b247a68ac
Ray Ching <71825980+LILRAY0826@users.noreply.github.com>
2021-12-04 03:09:49 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -3,46 +3,62 @@ import numpy as np
 import model
 import matplotlib.pyplot as plt
 
-# Load voc img
+# Load voc and original image
 voc_img = cv2.imread("label_to_voc_dataset/voc_dataset/SegmentationClassPNG/full_duck.png")
 org_img = cv2.imread("label_to_voc_dataset/label_dataset/full_duck.jpeg")
 
 '''''''''
 # Image cutting for testing
-voc_img = voc_img[1500:2000, 1000:3000]
-org_img = org_img[1500:2000, 1000:3000]
+voc_img = voc_img[1500:2000, 2000:3000]
+org_img = org_img[1500:2000, 2000:3000]
 '''''''''
 
-duck_pixels, non_duck_pixels = model.find_duck_and_non_duck_pixels(voc_img)
-duck_rgb, non_duck_rgb = model.find_duck_and_non_duck_rgb(org_img, duck_pixels, non_duck_pixels)
+duck_pixels, water_pixels, grass_pixels, road_pixels, sand_pixels = model.find_pixels(voc_img)
+duck_rgb, water_rgb, grass_rgb, road_rgb, sand_rgb = model.find_rgb(org_img, duck_pixels, water_pixels, grass_pixels, road_pixels, sand_pixels)
 
 print("Training, please wait a second.")
 # Get the feature vector of duck and non_duck
 mean_duck_rgb = model.mean(duck_rgb)
-mean_non_duck_rgb = model.mean(non_duck_rgb)
+mean_water_rgb = model.mean(water_rgb)
+mean_grass_rgb = model.mean(grass_rgb)
+mean_road_rgb = model.mean(road_rgb)
+mean_sand_rgb = model.mean(sand_rgb)
 
 # Get the sigma vector of duck and non_duck
 sigma_duck_rgb = model.sigma(duck_rgb, mean_duck_rgb)
-sigma_non_duck_rgb = model.sigma(non_duck_rgb, mean_non_duck_rgb)
+sigma_water_rgb = model.sigma(water_rgb, mean_water_rgb)
+sigma_grass_rgb = model.sigma(grass_rgb, mean_grass_rgb)
+sigma_road_rgb = model.sigma(road_rgb, mean_road_rgb)
+sigma_sand_rgb = model.sigma(sand_rgb, mean_sand_rgb)
 
 # Predict Likelihood
+predict_img = np.zeros([org_img.shape[0], org_img.shape[1], 3])
 for i in range (0, org_img.shape[0]):
     for j in range (0, org_img.shape[1]):
         x_array = np.array([org_img[i][j]])
         x_array = x_array.T
         print('Predicting : {:d}/{:d}, {:d}/{:d}'.format(i, org_img.shape[0], j, org_img.shape[1]))
         possibility_of_duck = model.likelihood(x_array, mean_duck_rgb, sigma_duck_rgb)
-        possibility_of_non_duck = model.likelihood(x_array, mean_non_duck_rgb, sigma_non_duck_rgb)
-        if possibility_of_duck[0] >= possibility_of_non_duck[0]:
-            org_img[i][j] = [255, 255, 255]
-        else :
-            org_img[i][j] = [0, 0, 0]
+        possibility_of_water = model.likelihood(x_array, mean_water_rgb, sigma_water_rgb)
+        possibility_of_grass = model.likelihood(x_array, mean_grass_rgb, sigma_grass_rgb)
+        possibility_of_road = model.likelihood(x_array, mean_road_rgb, sigma_road_rgb)
+        possibility_of_sand = model.likelihood(x_array, mean_sand_rgb, sigma_sand_rgb)
+        if max([possibility_of_duck, possibility_of_water, possibility_of_grass, possibility_of_road, possibility_of_sand]) == possibility_of_duck :
+            predict_img[i][j] = [255, 255, 255]
+        else:
+            predict_img[i][j] = [0, 0, 0]
+        
 
-plt.subplot(2, 1, 1)
+# Show the predict result and compare with the original image
+plt.subplot(3, 1, 1)
+plt.title("org_img")
+plt.imshow(org_img)
+
+plt.subplot(3, 1, 2)
 plt.title("voc_img")
 plt.imshow(voc_img)
 
-plt.subplot(2, 1, 2)
+plt.subplot(3, 1, 3)
 plt.title("predict_img")
-plt.imshow(org_img)
+plt.imshow(predict_img)
 plt.show()
--- model.py
+++ model.py
@@ -1,49 +1,84 @@
 import numpy as np
 
 # from voc data find pixels of duck & non_duck
-def find_duck_and_non_duck_pixels(voc_img):
+def find_pixels(voc_img):
     # declare two lists to put in the pixels of labels
     duck_pixels = []
-    non_duck_pixels = []
+    water_pixels = []
+    grass_pixels = []
+    road_pixels = []
+    sand_pixels = []
     # find duck and non_duck pixels, then append into the related list
     for i in range(0, voc_img.shape[0]):
         for j in range(0, voc_img.shape[1]):
             print("Collecting and Scanning Pixels : {:d}/{:d}, {:d}/{:d}".format(i, voc_img.shape[0], j, voc_img.shape[1]))
-            if (voc_img[i][j] == [0, 0, 128]).all():  # is duck pixels
+            if (voc_img[i][j] == [0, 0, 128]).all():  # is duck pixels BGR
                 a = [i, j]
                 duck_pixels.append(a)
-            if (voc_img[i][j] == [0, 128, 0]).all():  # is non_duck pixels
+            elif (voc_img[i][j] == [0, 128, 128]).all():  # is water pixels
                 b = [i, j]
-                non_duck_pixels.append(b)
-    return duck_pixels, non_duck_pixels
+                water_pixels.append(b)
+            elif (voc_img[i][j] == [128, 0, 0]).all():  # is grass pixels
+                c = [i, j]
+                grass_pixels.append(c)
+            elif (voc_img[i][j] == [128, 0, 128]).all():  # is road pixels
+                d = [i, j]
+                road_pixels.append(d)
+            elif (voc_img[i][j] == [128, 128, 0]).all():  # is sand pixels
+                f = [i, j]
+                sand_pixels.append(f)
+    return duck_pixels, water_pixels, grass_pixels, road_pixels, sand_pixels
 
 # from pixels find the rgb values of duck & non_duck
-def find_duck_and_non_duck_rgb(org_img, duck_pixels, non_duck_pixels):
+def find_rgb(org_img, duck_pixels, water_pixels, grass_pixels, road_pixels, sand_pixels):
     # declare two lists to put in the rgb values of labels
     duck_rgb = []
-    non_duck_rgb = []
+    water_rgb = []
+    grass_rgb = []
+    road_rgb = []
+    sand_rgb = []
     # find duck and non_duck rgb values, then append into the related list
     for i in range(0, len(duck_pixels)):
         print('Get duck RGB : {:d}/{:d}'.format(i, len(duck_pixels)))
         x = duck_pixels[i][0]
         y = duck_pixels[i][1]
         duck_rgb.append(org_img[x][y])
-    for i in range(0, len(non_duck_pixels)):
-        print('Get non duck RGB : {:d}/{:d}'.format(i, len(non_duck_pixels)))
-        x = non_duck_pixels[i][0]
-        y = non_duck_pixels[i][1]
-        non_duck_rgb.append(org_img[x][y])
+    for i in range(0, len(water_pixels)):
+        print('Get non duck RGB : {:d}/{:d}'.format(i, len(water_pixels)))
+        x = water_pixels[i][0]
+        y = water_pixels[i][1]
+        water_rgb.append(org_img[x][y])
+    for i in range(0, len(grass_pixels)):
+        print('Get non duck RGB : {:d}/{:d}'.format(i, len(grass_pixels)))
+        x = grass_pixels[i][0]
+        y = grass_pixels[i][1]
+        grass_rgb.append(org_img[x][y])
+    for i in range(0, len(road_pixels)):
+        print('Get non duck RGB : {:d}/{:d}'.format(i, len(road_pixels)))
+        x = road_pixels[i][0]
+        y = road_pixels[i][1]
+        road_rgb.append(org_img[x][y])
+    for i in range(0, len(sand_pixels)):
+        print('Get non duck RGB : {:d}/{:d}'.format(i, len(sand_pixels)))
+        x = sand_pixels[i][0]
+        y = sand_pixels[i][1]
+        sand_rgb.append(org_img[x][y])
 
-    return duck_rgb, non_duck_rgb
+    return duck_rgb, water_rgb, grass_rgb, road_rgb, sand_rgb
 
 # Mean of feature vector (RGB value)
 def mean(array):
     mean_feature_vector = [0, 0, 0]
     for i in range (0, len(array)):
         mean_feature_vector += array[i]
-    mean_feature_vector[0] = mean_feature_vector[0] / len(array)
-    mean_feature_vector[1] = mean_feature_vector[1] / len(array)
-    mean_feature_vector[2] = mean_feature_vector[2] / len(array)
+    if len(array) == 0:
+        mean_feature_vector[0] = 0
+        mean_feature_vector[1] = 0
+        mean_feature_vector[2] = 0
+    else :
+        mean_feature_vector[0] = mean_feature_vector[0] / len(array)
+        mean_feature_vector[1] = mean_feature_vector[1] / len(array)
+        mean_feature_vector[2] = mean_feature_vector[2] / len(array)
     mean_feature_vector = np.array([mean_feature_vector])
     return mean_feature_vector.T
 
@@ -56,13 +91,21 @@ def sigma(rgb_array, mean_array):
         x_array = np.array([rgb_array[i]])
         x_array = x_array.T
         sigma_vector += (x_array - mean_array) * ((x_array - mean_array).T)
-    sigma_vector = sigma_vector/(len(rgb_array) - 1)
+    if len(rgb_array) != 0:
+        sigma_vector = sigma_vector / (len(rgb_array))
     return sigma_vector
 
 # Predict Likelihood
 def likelihood (feature_vector, mean_vector, sigma_vector):
-    constant = 1 / ((2*np.pi)**(3/2)*(np.linalg.det(sigma_vector))**(1/2))
-    np_linalg_inv = np.linalg.inv(sigma_vector)
-    feature_vector_minus_mean_vector = feature_vector - mean_vector
-    exponential = np.exp((-0.5) * np.dot(np.dot((feature_vector_minus_mean_vector.T), np_linalg_inv), feature_vector_minus_mean_vector))
+    if np.linalg.det(sigma_vector) != 0 :
+        constant = 1 / ((2 * np.pi) ** (3 / 2) * (np.linalg.det(sigma_vector)) ** (1 / 2))
+    else :
+        constant = 0
+    sigma_vector = np.array(sigma_vector)
+    if sigma_vector.all() != 0 :
+        np_linalg_inv = np.linalg.inv(sigma_vector)
+        feature_vector_minus_mean_vector = feature_vector - mean_vector
+        exponential = np.exp((-0.5) * np.dot(np.dot((feature_vector_minus_mean_vector.T), np_linalg_inv),feature_vector_minus_mean_vector))
+    else :
+        exponential = 0
     return constant*exponential
